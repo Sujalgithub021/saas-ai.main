dfd0a1ce101e7e2c60f1c88aa503b4ba1af26f2c
sujal <raisujal021@gmail.com>
2026-03-10 00:03:19 +0530

    fix cors

--- server/server.js
+++ server/server.js
@@ -11,7 +11,12 @@ const app = express()
 
 await connectCloudinary()
 
-app.use(cors())
+app.use(cors({
+  origin: [
+    'https://saas-ai-main.vercel.app',
+    'http://localhost:5173'
+  ]
+}))
 app.use(express.json())
 app.use(clerkMiddleware())
 
